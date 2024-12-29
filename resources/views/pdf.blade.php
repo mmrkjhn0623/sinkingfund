9578b910857b0facf825610312af161303fb0eb6
mmrkjhn <mmrkjhn@gmail.com>
2024-12-29 14:33:05 +0800

    update 12/29/24

---
+++ resources/views/pdf.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <table class="table table-bordered">
+    <thead>
+      <tr>
+        <td><b>Company Name</b></td>
+        <td><b>Department Name</b></td>
+        <td><b>Team Lead name</b></td>     
+      </tr>
+      </thead>
+      <tbody>
+      <tr>
+        <td>
+          Mark John Mondares
+        </td>
+        <td>
+          Web Developer
+        </td>
+        <td>
+          mmrkjhn@gmail
+        </td>
+      </tr>
+      </tbody>
+    </table>
+  </body>
+</html>
